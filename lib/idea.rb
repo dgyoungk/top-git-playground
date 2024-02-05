84e49cdbbd3117790f18cbf8ed35f1ec5507420d
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-02-05 03:30:45 -0800

    Remove unnecessary text

--- lib/idea.rb
+++ lib/idea.rb
@@ -2,12 +2,9 @@ puts %(Hello world!)
 
 langs = %w(Ruby Python Javascript C# Java)
 
-<<<<<<< HEAD
 langs.each { |lang| puts lang }
-=======
 puts %(This line was added in the main branch)
 
 catchphrase = %w(wubba lubba dub dub)
 
 catchphrase.each { |word| puts word }
->>>>>>> develop
